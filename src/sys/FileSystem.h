36a45cb5876a27397538b639fd949f81ffa19105
Dzadaafa <dzadafa77@gmail.com>
2025-12-11 22:34:56 +0700

    refactor: separating the logic

---
+++ src/sys/FileSystem.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <windows.h>
+
+struct FileEntry {
+    std::wstring name;
+    std::wstring fullPath;
+    bool isDirectory;
+    int iconIndex;
+    DWORD attributes;
+};
+
+class FileSystem {
+public:
+    static std::vector<FileEntry> ScanDirectory(const std::wstring& directory);
+    static int GetFileIconIndex(const std::wstring& path, DWORD attributes);
+};
